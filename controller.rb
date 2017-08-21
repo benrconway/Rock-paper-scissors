8bec9e27ecc6f51c647f25f5aee1ce1bc615611b
Benjamin Conway <benrconway84@gmail.com>
2017-08-21 21:24:05 +0100

    Add some CSS and refactor Game a little.

--- controller.rb
+++ controller.rb
@@ -4,7 +4,11 @@ require_relative("models/game.rb")
 
 also_reload("./models/*")
 
-get "/:player1/:player2" do
+get "/game/:player1/:player2" do
   @round = Game.begin(params[:player1], params[:player2])
   erb(:result)
+end
+
+get "/" do
+  erb(:welcome)
 end
--- models/game.rb
+++ models/game.rb
@@ -1,20 +1,20 @@
 class Game
 
   def Game.begin(hand1, hand2)
-    if hand1 == "rock" && hand2 == "rock"
-      return "It's a tie of Rocks!"
+    if hand1 == hand2
+      return "It's a tie of #{hand1.capitalize}-tastic proportions!"
     elsif hand1 == "rock" && hand2 == "scissors"
-      return "Player 1 wins, SMASHING Player 2's scissors!"
+      return "Player 1 wins by SMASHING Player 2's scissors!"
     elsif hand1 == "rock" && hand2 == "paper"
       return "Player 2 wins by neatly wrapping up Player 1's rock in his paper!"
     elsif hand1 == "scissors" && hand2 == "rock"
       return "Player 2 wins by SMASHING Player 1's scissors!"
     elsif hand1 == "paper" && hand2 == "rock"
       return "Player 1 wins by neatly wrapping up Player 2's rock in his paper!"
-    elsif hand1 == "scissors" && hand2 == "scissors"
-      return "It's a tie of Scissors!"
-    else hand1 == "paper" && hand2 == "paper"
-      return "It's a tie of Oragamic proportions!"
+    elsif hand1 == "paper" && hand2 == "scissors"
+      return "Player 2 has won by escaping the paper dungeon with their Scissors of Fury!"
+    else hand1 == "scissors" && hand2 == "paper"
+      return "Player 1 has escaped the paper dungeon with their Scissors of Fury!"
     end
   end
 
